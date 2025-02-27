23324d8747deeba2ab7f6e1c4c69b67ee96444ee
Lenka Mudrich <lenka.mudrich@gmail.com>
2025-02-27 16:24:18 +0000

    Pridan template pro druhy ukol

---
+++ lekce_2/du2_template.py
@@ -0,0 +1,7 @@
+from microbit import display
+
+class Obrazovka:
+    def vypis(text):
+        # zde implementujte tuto funkci tak,
+        # aby vstupni "text" vypsala jak na microbitu,
+        # tak v terminalu na pocitaci
