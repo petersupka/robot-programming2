c1862660882adb268faa958965391b4cd7130eb9
Lenka Mudrich <lenka.mudrich@gmail.com>
2025-03-04 18:28:50 +0000

    Pridan kod na rozjeti robota

--- lekce_3/live.py
+++ lekce_3/live.py
@@ -1,10 +1,26 @@
-from microbit import i2c
+from microbit import i2c, sleep
 
-if __name__ == "__main__":
-    # Write your code here :-)
-    i2c.init()
-
+def vycti_adresy():
     adresy = i2c.scan()
 
     for adresa in adresy:
         print(hex(adresa))
+
+    print("konec")
+    print(len(adresy))
+
+def init_motoru():
+    i2c.write(0x70, b'\x00\x01')
+    i2c.write(0x70, b'\xE8\xAA')
+    sleep(100)
+
+def jed(rychlost):
+    i2c.write(0x70, b'\x03' + bytes([rychlost]))
+
+if __name__ == "__main__":
+    # Write your code here :-)
+    i2c.init()
+    init_motoru()
+    jed(135) # 0 - 255
+    sleep(1000)
+    jed(0)
