f63570e545e5fbe966e8102d997cfbaadabd3470
Peter Supka <supkapeter@gmail.com>
2025-03-12 22:31:20 +0100

    Upravena verzia pre DU4 podla template

--- lekce_4/du4_template_EN.py
+++ lekce_4/du4_template_EN.py
@@ -1,16 +1,20 @@
 from microbit import sleep, pin14, pin15
 
+sum_ticks_dict = {"right": {"sum_ticks": 0, "previous_pos": 0}, "left": {"sum_ticks": 0, "previous_pos": 0}}
+
 def left_sum_ticks():
     surova_data_leva = pin14.read_digital()
-    #zde napiste vas kod
-    #scitejte tiky pro levy enkoder od zacatku behu progamu
-    return #vratte soucet
+    if sum_ticks_dict["left"]["previous_pos"] != surova_data_leva:
+        sum_ticks_dict["left"]["sum_ticks"] += 1
+        sum_ticks_dict["left"]["previous_pos"] = surova_data_leva
+    return sum_ticks_dict["left"]["sum_ticks"]
 
 def right_sum_ticks():
     surova_data_prava = pin15.read_digital()
-    #zde napiste vas kod
-    #scitejte tiky pro pravy enkoder od zacatku behu progamu
-    return #vratte soucet
+    if sum_ticks_dict["right"]["previous_pos"] != surova_data_prava:
+        sum_ticks_dict["right"]["sum_ticks"] += 1
+        sum_ticks_dict["right"]["previous_pos"] = surova_data_prava
+    return sum_ticks_dict["right"]["sum_ticks"]
 
 if __name__ == "__main__":
 
