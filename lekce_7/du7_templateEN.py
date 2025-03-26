b13c226033fb809285a3ffe764b361b0970341f3
Peter Supka <supkapeter@gmail.com>
2025-03-27 00:23:37 +0100

    Vypocet v_l a v_r, predanie parametrov do go_pwm + pohyb robota

--- lekce_7/du7_templateEN.py
+++ lekce_7/du7_templateEN.py
@@ -1,9 +1,11 @@
-from picoed import i2c
+from picoed import i2c, button_a, display
 from time import sleep
 
+d = 0.075 # distance in meters between wheel center and robot center
+
 def init_motor():
-    i2c.write(0x70, bytes([0, 0x01]))
-    i2c.write(0x70, bytes([8, 0xAA]))
+    i2c.writeto(0x70, bytes([0, 0x01]))
+    i2c.writeto(0x70, bytes([8, 0xAA]))
     sleep(0.01)
 
 def go(forward, angular):
@@ -14,6 +16,19 @@ def go(forward, angular):
     # Použijte vzorečky kinematiky a spočítejte v_l a v_r
     # Podle znamínek v_l a v_r volejte příslušné příkazy na směr motorů
     # Metoda také zastaví pokud ji dám nulové rychlosti
+
+    v_l = int(forward - d * angular)
+    v_r = int(forward + d * angular)
+
+    if (v_l >= 0):
+        go_pwm("left", "forward", v_l)
+    else:
+        go_pwm("left", "backward", abs(v_l))
+
+    if (v_r >= 0):
+        go_pwm("right", "forward", v_r)
+    else:
+        go_pwm("right", "backward", abs(v_r))
 
     return 0
 
@@ -42,8 +57,8 @@ def set_canals(canal_off, canal_on, pwm):
     while not i2c.try_lock():
         pass
     try:
-        i2c.write(0x70, bytes([canal_off, 0]))
-        i2c.write(0x70, bytes([canal_on, pwm]))
+        i2c.writeto(0x70, bytes([canal_off, 0]))
+        i2c.writeto(0x70, bytes([canal_on, pwm]))
     finally:
         i2c.unlock()
     return 0
@@ -56,3 +71,18 @@ if __name__ == "__main__":
     # Zastaveni 1s - DULEZITE! Nikdy nemente smer jizdy bez zastaveni
     # Otáčení robota na místě doleva
     # zastaveni
+
+    display.show('A')   # Menu
+
+    while not button_a.was_pressed():
+        sleep(0.1)
+        pass
+
+    go(135, 0)  # dopredu
+    sleep(1)
+    go(0, 0)    # zastavenie
+    sleep(1)
+    go(0, 1350) # otocenie vlavo
+    sleep(1)
+    go(0, 0)    # zastavenie
+    sleep(1)
